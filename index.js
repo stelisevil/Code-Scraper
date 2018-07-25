f00e1cfe20188f4833eec3d609f17238c8240c98
Stelios Tsinontas <stelisevil@hotmail.com>
2018-07-25 22:29:04 +0100

    Added scraper functionality
    Added ability to scrape emails, phones and places. removed dates.

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // Code Scraper
 
-// calling the knwl.js module to extract dates from the provided string
+// calling the knwl.js module to extract information from the provided string
 var Knwl = require("./node_modules/knwl.js");
 var knwlInstance = new Knwl('english');
-knwlInstance.register('dates', require('./node_modules/knwl.js/default_plugins/dates.js'));
+
+knwlInstance.register('emails', require('./node_modules/knwl.js/default_plugins/emails.js'));
+knwlInstance.register('phones', require('./node_modules/knwl.js/default_plugins/phones.js'));
+knwlInstance.register('places', require('./node_modules/knwl.js/default_plugins/places.js'));
 
 // The string below. I've taken the example provided in the knwl README.md file to test
-knwlInstance.init('This was written on the 2nd of June, of 2015.');
+knwlInstance.init('This was written on the 2nd of June, of 2015 and my email address is name@company.com');
 
-var dates = knwlInstance.get('dates');
+var emails = knwlInstance.get('emails');
+var phones = knwlInstance.get('phones');
+var places = knwlInstance.get('places');
 
-console.log(dates);
+console.log(emails);
+console.log(phones);
+console.log(places);
