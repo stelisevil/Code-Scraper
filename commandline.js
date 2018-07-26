5a6892c9ea2c8b5da3320ea7d1b7ad2b4007df57
Stelios Tsinontas <stelisevil@hotmail.com>
2018-07-26 04:38:32 +0100

    added commandline.js
    Created commandline.js to accept a parameter and also performs a validation check to confirm it is an email.
    Added comments to emailtodomain.js.
    Created htmltotext.js to begin work on stripping out html tags from source code.

---
+++ commandline.js
@@ -0,0 +1,14 @@
+// Takes an email address provided on the command line and sets it as a variable
+
+var commandLineEmail = process.argv.slice(2);
+
+function validateEmail(commandLineEmail) {
+  var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(commandLineEmail);
+}
+
+if (validateEmail(commandLineEmail)) {
+  console.log(`You entered: ${commandLineEmail}`);
+} else {
+  console.log(`Error: ${commandLineEmail} is not a valid email address.`);
+};
--- emailtodomain.js
+++ emailtodomain.js
@@ -1,7 +1,9 @@
 // A simple function for converting an email address into a domain name
 
+// test email
 let email = 'stelisevil@hotmail.com'
 
+// A way of finding where the @ symbol is in the email address
 let atSymbol = () => {
   for (atSymbolIndex = 0; atSymbolIndex < email.length; atSymbolIndex++) {
     if (email[atSymbolIndex] === '@') {
@@ -16,6 +18,8 @@ console.log(email[atSymbolIndex])
 
 console.log(email.substring(atSymbolIndex+1))
 
+
+// creates the domain name as a string and saves it to the variable 'domain'
 domain = 'http://www.'.concat(email.substring(atSymbolIndex+1))
 
 console.log(domain)
